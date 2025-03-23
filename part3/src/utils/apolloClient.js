f8a1de6dd9b2d95b92b1c8f8ec49089516fbf5c4
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2025-03-23 21:57:21 +0200

    10.12

--- part3/src/utils/apolloClient.js
+++ part3/src/utils/apolloClient.js
@@ -1,10 +1,13 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import Constants from 'expo-constants';
 
 
 const createApolloClient = () => {
+  const ip = Constants.expoConfig.extra.env;
+  console.log(`ip: ${ip}`);
 
   return new ApolloClient({
-    uri: `${process.env.REACT_APP_MYAPP_URI}:4000/graphql`,
+    uri: `${ip}:4000/graphql`,
     cache: new InMemoryCache(),
   });
 };
