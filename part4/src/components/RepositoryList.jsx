b822f6cfeff26b7d4efcf2db922fb6ca8b293bee
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2025-04-07 20:24:01 +0300

    Add picker-version

--- part4/src/components/RepositoryList.jsx
+++ part4/src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { FlatList, View, StyleSheet, Pressable } from 'react-native';
 import { useNavigate } from 'react-router-native';
+import { Picker } from '@react-native-picker/picker';
 
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
@@ -19,37 +20,65 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryListFirstContainer = ({ repositories }) => {
-  const navigate = useNavigate();
 
-  return <RepositoryListContainer repositories={repositories} navigate={navigate} />;
-};
+export const RepositoryListContainer = ({ repositories, navigate, sortRepos, setSortRepos, menuVisible, setMenuVisible }) => {
 
-export const RepositoryListContainer = ({ repositories, navigate }) => {
+  const openPickerMenu = () => setMenuVisible(true);
+  const closePickerMenu = () => setMenuVisible(false);
+
+  const handleSort = (howToSort) => {
+    setSortRepos(howToSort);
+    closePickerMenu();
+  };
 
   // Get the nodes from the edges array
   const repositoryNodes = repositories
-    ? repositories.edges.map((edge) => edge.node)
+    ? repositories?.edges?.map((edge) => edge.node)
     : [];
 
   return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item }) =>
-        <Pressable onPress={() => navigate(`/repos/${item.id}`)}>
-          <RepositoryItem item={item} />
-        </Pressable >}
-      keyExtractor={item => item.id}
-      contentContainerStyle={styles.listContainer}
-    />
+    <View style={{ flex: 1, paddingHorizontal: 10 }}>
+      <Picker
+        selectedValue={sortRepos}
+        onValueChange={(itemValue) => setSortRepos(itemValue)}
+        style={{ marginBottom: 10 }}
+      >
+        <Picker.Item label="Latest repositories" value="default" />
+        <Picker.Item label="Highest rated repositories" value="highRated" />
+        <Picker.Item label="Lowest rated repositories" value="lowRated" />
+      </Picker>
+
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={({ item }) =>
+          <Pressable onPress={() => navigate(`/repos/${item.id}`)}>
+            <RepositoryItem item={item} />
+          </Pressable >}
+        keyExtractor={item => item.id}
+        contentContainerStyle={styles.listContainer}
+      />
+    </View>
   );
 };
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
 
-  return <RepositoryListFirstContainer repositories={repositories} />;
+const RepositoryList = () => {
+  const [ sortRepos, setSortRepos ] = useState();
+  const { repositories } = useRepositories(sortRepos);
+  const navigate = useNavigate();
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  return (
+    <RepositoryListContainer
+      repositories={repositories}
+      sortRepos={sortRepos}
+      setSortRepos={setSortRepos}
+      navigate={navigate}
+      menuVisible={menuVisible}
+      setMenuVisible={setMenuVisible}
+    />
+  )
 };
 
 
